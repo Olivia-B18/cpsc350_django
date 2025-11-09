b52549fa22a80dbdeb04719ee68961b2129c7653
oliviab <ofbuchanan@icloud.com>
2025-11-09 16:18:27 -0500

    Commit hlydell's pull request

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -13,7 +13,6 @@ def index(request):
 def topics(request):
     """show all topics"""
     topics = Topic.objects.filter(owner=request.user).order_by('date_added')
-    topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
